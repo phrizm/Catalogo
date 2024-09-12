2d8878ae60610b78081437ef9e6ea843bd5e4fb4
Richard <56654610+phrizm@users.noreply.github.com>
2024-09-12 17:31:34 +0200

    Initial Repo

---
+++ www/src/components/accessori.php
@@ -0,0 +1,11 @@
+
+<section class="p-2 border border-dashed rounded-md border-brand">
+<div class="flex flex-row justify-center w-full h-4 p-2 rounded-md accessori bg-surfacedark place-items-center">
+<h2 class="font-bold text-2xs font-hubot">Accessori | Accessories | Accessoires | Zubehör | Accesorios</h2>
+</div>
+
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et culpa reprehenderit aliquam, magnam quos sequi hic saepe animi tenetur beatae accusantium obcaecati velit, cupiditate officiis ipsa sit, nostrum deleniti aperiam esse officia. Consectetur dolores aperiam amet quaerat doloribus, inventore molestias! Dolores libero aut tempora esse sequi? Et nostrum deleniti recusandae iure optio deserunt autem placeat dolores porro, blanditiis nisi delectus obcaecati fugit sequi maxime exercitationem ratione, eius repudiandae eum omnis expedita possimus! Sapiente quod sunt soluta non velit quam aperiam illum dolor. Recusandae maxime odio cum, quidem voluptatibus commodi consectetur non, aspernatur, veniam nihil libero dolore? Voluptate vitae inventore incidunt!</p>
+</section>
+
+
